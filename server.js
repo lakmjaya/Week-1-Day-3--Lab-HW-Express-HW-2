d3effe6278b7b7054b7be8eb5dc37436703bf57c
DunstWalmart GitHub <lakmjaya@gmail.com>
2023-01-17 10:46:56 -0500

    Final

--- server.js
+++ server.js
@@ -38,6 +38,15 @@ app.get('/magic/:Will%20I%20Be%20A%20Millionaire', function(req, res) {
     }
 });
 
+app.get('/magic/:question', function(req, res) {
+    let answer=["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    res.send(`Will I be a Millionaire ?  <h1>${answer[getRandomInt(21)] } </h1>`);
+    console.log(answer[getRandomInt(21)])
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+});
+
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
  console.log('Listening on port 3000');
